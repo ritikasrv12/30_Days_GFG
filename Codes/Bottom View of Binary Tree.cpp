aa44a417418d5c39be9f97592aaa60734a7688bc
Ritika Srivastava <105850960+ritikasrv12@users.noreply.github.com>
2024-08-05 14:53:40 +0530

    Bottom View of Binary Tree.cpp

---
+++ Codes/Bottom View of Binary Tree.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    vector <int> bottomView(Node *root) {
+          map<int, int>mp;
+        queue<pair<int, Node*>>q;
+        q.push({0, root});
+        
+        while(!q.empty()){
+            int level=q.front().first;
+            Node* node=q.front().second;
+            mp[level]=node->data;
+            q.pop();
+            
+            if(node->left){
+                q.push({level-1, node->left});
+            }
+            if(node->right){
+                q.push({level+1, node->right});
+            }
+        }
+        
+        vector<int>ans;
+        for(auto it:mp){
+            ans.push_back(it.second);
+        }
+        return ans;
+        // Your Code Here
+    }
+};
